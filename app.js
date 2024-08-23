b6e1941abbf19e326f847e08f0ca1b63786c7a83
Saeyoung Park <spark363@my.bcit.ca>
2024-08-23 10:27:37 -0700

    Add mouse event to draw lines

--- app.js
+++ app.js
@@ -6,28 +6,52 @@ canvas.height = 800;
 const brush = canvas.getContext("2d");
 
 const colors = [
+    "#eccc68",
     "##ffa502",
     "#ff7f50",
     "#ff6348",
     "#ff4757",
+    "#ff6b81",
+    "#7bed9f",
     "#2ed573",
+    "#70a1ff",
     "#1e90ff",
+    "#5352ed",
     "#3742fa",
-    "#a4b0be",
-    "#2f3542",
+    "#ffffff",
     "#f1f2f6",
+    "#dfe4ea",
+    "#ced6e0",
+    "#a4b0be",
+    "#747d8c",
+    "#57606f",
+    "#2f3542",
 ]
 
 brush.lineWidth = 2;
+let isPainting = false;
 
-function onclick(event) {
-    brush.beginPath();
-    brush.moveTo(0, 0);
+function onMove(event) {
+    if (isPainting) {
+        brush.lineTo(event.offsetX, event.offsetY);
+        brush.stroke();
+        return;
+    }
 
-    const color = colors[Math.floor(Math.random() * colors.length)];
-    brush.strokeStyle = color;
-
-    brush.lineTo(event.offsetX, event.offsetY);
-    brush.stroke();
+    brush.moveTo(event.offsetX, event.offsetY);
 }
-canvas.addEventListener("mousemove", onclick);
+
+function startPainting() {
+    isPainting = true;
+}
+
+function cancelPainting() {
+    isPainting = false;
+}
+
+canvas.addEventListener("mousemove", onMove);
+canvas.addEventListener("mousedown", startPainting);
+
+canvas.addEventListener("mouseup", cancelPainting);
+canvas.addEventListener("mouseleave", cancelPainting); // monitor whether the mouse pointer leave the canvas
+
