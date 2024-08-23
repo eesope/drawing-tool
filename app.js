246cc026832c5ef3c2dc1979759b7460545a0288
Saeyoung Park <spark363@my.bcit.ca>
2024-08-23 12:08:44 -0700

    Add color pallet for stroke/fill

--- app.js
+++ app.js
@@ -1,35 +1,19 @@
 const canvas = document.querySelector("canvas");
+const lineWidth = document.getElementById("line-width");
+const color = document.getElementById("color");
+const colorOptions = Array.from(document.getElementsByClassName("color-option")); // make array to use forEach 
+const modeBTN = document.getElementById("mode-btn");
+
 canvas.width = 800;
 canvas.height = 800;
 
 // painting brush
 const brush = canvas.getContext("2d");
 
-const colors = [
-    "#eccc68",
-    "##ffa502",
-    "#ff7f50",
-    "#ff6348",
-    "#ff4757",
-    "#ff6b81",
-    "#7bed9f",
-    "#2ed573",
-    "#70a1ff",
-    "#1e90ff",
-    "#5352ed",
-    "#3742fa",
-    "#ffffff",
-    "#f1f2f6",
-    "#dfe4ea",
-    "#ced6e0",
-    "#a4b0be",
-    "#747d8c",
-    "#57606f",
-    "#2f3542",
-]
+brush.lineWidth = lineWidth.value;
 
-brush.lineWidth = 2;
 let isPainting = false;
+let isFilling = false;
 
 function onMove(event) {
     if (isPainting) {
@@ -47,6 +31,40 @@ function startPainting() {
 
 function cancelPainting() {
     isPainting = false;
+    brush.beginPath();
+}
+
+function onLineWidthChange(event) {
+    brush.lineWidth = event.target.value;
+}
+
+function onColorChange(event) {
+    brush.strokeStyle = event.target.value;
+    brush.fillStyle = event.target.value;
+}
+
+function onColorClick(event) {
+    const colorValue = event.target.dataset.color
+    brush.strokeStyle = colorValue;
+    brush.fillStyle = colorValue;
+    color.value = colorValue;
+}
+
+function onModeClick() {
+    if (isFilling) {
+        isFilling = false;
+        modeBTN.innerText = "Fill"
+    } else {
+        isFilling = true;
+        modeBTN.innerText = "Draw"
+    }
+
+}
+
+function onCanvasClick() {
+    if (isFilling) {
+        brush.fillRect(0, 0, 800, 800)
+    }
 }
 
 canvas.addEventListener("mousemove", onMove);
@@ -54,4 +72,11 @@ canvas.addEventListener("mousedown", startPainting);
 
 canvas.addEventListener("mouseup", cancelPainting);
 canvas.addEventListener("mouseleave", cancelPainting); // monitor whether the mouse pointer leave the canvas
+canvas.addEventListener("click", onCanvasClick);
 
+lineWidth.addEventListener("change", onLineWidthChange);
+
+color.addEventListener("change", onColorChange);
+colorOptions.forEach(color => color.addEventListener("click", onColorClick));
+
+modeBTN.addEventListener("click", onModeClick);
